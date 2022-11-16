7e7c5e1d03861290032ac21ca3eea95abfc198b2
sshabnam02 <117852474+sshabnam02@users.noreply.github.com>
2022-11-16 21:15:06 +0530

    Update COVID Portfolio Project actual script (SQL query).sql

--- COVID Portfolio Project actual script (SQL query).sql
+++ COVID Portfolio Project actual script (SQL query).sql
@@ -1,8 +1,15 @@
+/*
+Covid 19 Data Exploration
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
 
 SELECT * 
 FROM [dbo].[Covid Deaths]
 WHERE continent is not null
-ORDER BY 3,4;
+ORDER BY 3,4
+
 
 SELECT *
 FROM [dbo].[Covid Vaccinations]
@@ -15,33 +22,40 @@ FROM [dbo].[Covid Deaths]
 WHERE continent is not null
 ORDER BY 1,2;
 
+
 -- Looking at Total Cases vs Total Deaths
 -- Shows the lkelihood of dying if you contractcovid in your country
+
 SELECT Location, date,total_cases,total_deaths, (total_deaths/total_cases)*100 AS Deathpercentage
 FROM [dbo].[Covid Deaths]
 WHERE location like '%states%'
 and continent is not null
 ORDER BY 1,2;
 
--- Looking at the total cases vs Population
--- shows what percentage of population got covid
+-- Looking at the Total Cases vs Population
+-- Shows what percentage of population Infected with covid
 
 SELECT Location, date,total_cases,population, (total_cases/population)*100 AS Deathpercentage
 FROM [dbo].[Covid Deaths]
 -- WHERE location like '%states%
 WHERE continent is not null
-ORDER BY 1,2;
+ORDER BY 1,2
 
--- Looking at countries with highest infection Rate compared to population
+
+-- Looking at Countries with Highest Infection Rate Compared to Population
+
 
 SELECT Location,population, MAX(total_cases) AS HighestInfectionCount,MAX((total_cases/population)) AS PercentPopulationInfected
 FROM [dbo].[Covid Deaths]
 -- WHERE location like '%states%'
 WHERE continent is not null
 GROUP BY Location, Population
-ORDER BY PercentPopulationInfected DESC;
+ORDER BY PercentPopulationInfected DESC
+
 
 -- Showing the Countries with the HighhestDeath Count per Population
+
+
 SELECT Location, MAX(cast(total_deaths as int)) AS TotalDeathCount
 FROM [dbo].[Covid Deaths]
 -- WHERE location like '%states%'
@@ -49,18 +63,22 @@ WHERE continent is not null
 GROUP BY Location
 ORDER BY TotalDeathCount DESC;
 
+
 -- LET'S BREAK THINGS DOWN BY CONTINENT
+
 
 SELECT continent, MAX(cast(total_deaths as int)) AS TotalDeathCount
 FROM [dbo].[Covid Deaths]
 -- WHERE location like '%states%'
 WHERE continent is not null
 GROUP BY continent
-ORDER BY TotalDeathCount DESC;
+ORDER BY TotalDeathCount DESC
+
+
 
 -- Showing the continents with the highest death per population
-
 -- GLOBAL NUMBERS
+
 
  SELECT SUM(new_cases) AS total_cases,SUM(CAST(new_deaths AS int)), SUM(CAST(new_deaths AS int))/ SUM(new_cases)*100 AS DeathPercentage
 --  (total_deaths/total_cases)*100 AS Deathpercentage
@@ -72,6 +90,8 @@ ORDER BY 1,2;
 
 
 --Looking at Total Population vs Vaccination
+--Shows Percentage of Population that has received at least one Covid Vaccination
+
 
 SELECT *
 FROM [dbo].[Covid Deaths] dea
@@ -98,7 +118,7 @@ JOIN [dbo].[Covid Vaccinations] vac
 WHERE dea.continent is not null
 ORDER BY 2,3; 
  
--- USE CTE
+-- USE CTE To perform Calculation on Partition By in previous Query
 
 With PopvsVac (continent, location, date, population,new_vaccination, RollingPeopleVaccinated)
 as
@@ -117,7 +137,7 @@ SELECT *,(RollingPeopleVaccinated/population)*100
 FROM PopvsVac
 
 
--- TEMP TABLE
+-- Using TEMP TABLE to perform Calculation on Partition By in previous Query
 
 
 DROP Table if exists #PercentPopulationVaccinated
